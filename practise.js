99f85f8a03f14a2d8d1c336aa9e6b7377d132025
salman naveed <realmadridinmydna3@gmail.com>
2023-01-10 18:43:24 +0500

    completed

---
+++ practise.js
@@ -0,0 +1,33 @@
+const series = [
+    {
+        name: 'The Wire',
+        location: 'Baltimore',
+        lengthInHours: 60,
+        genres: ['action', 'thriller', 'detective', 'suspense']
+    },
+    {
+        name: 'Game of Thromes',
+        location: 'Westeros and Essos',
+        lengthInHours: 70.25,
+        genres: ['fantasy', 'action', 'tragedy']
+    },
+    {
+        name: 'Friends',
+        location: 'New York',
+        lengthInHours: 85,
+        genres: ['comedy', 'romance', 'drama']
+    },
+    {
+        name: 'The Walking Dead',
+        location: 'Atlanta',
+        lengthInHours: 131,
+        genres: ['zombie', 'apocalypse', 'thriller', 'suspense']
+    },
+    {
+        name: 'The Big Bang Theory',
+        location: 'Pasadena',
+        lengthInHours: 139.66,
+        genres: ['comedy', 'nerd', 'romance']
+    },
+]
+
